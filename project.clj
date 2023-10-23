1da2a8e7ea5c7fc7636b508613e0aefe106ddbd9
Aaron Burdick <aaron.burdick@protonmail.com>
2023-10-23 23:15:48 +0900

    build: add deps for db, crypt

--- project.clj
+++ project.clj
@@ -6,12 +6,26 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.1.2"]
+                 [buddy/buddy-hashers "2.0.167"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [com.h2database/h2 "1.3.168"]
-                 [cheshire "4.0.3"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [cheshire "4.0.3"]
+                 [migratus "1.5.3"]
+                 [org.postgresql/postgresql "42.3.3"]
+                 [com.github.seancorfield/next.jdbc "1.3.894"]]
+  :plugins [[lein-ring "0.12.5"]
+            [migratus-lein "0.7.3"]]
   :ring {:handler real-world-clojure.handler/app}
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:dbtype "postgresql"
+                  :dbname "real_world"
+                  :host "localhost"
+                  :port "5433"
+                  :user "root"
+                  :password "root"}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]
+                        [org.clojure/java.jdbc "0.7.12"]]}})
