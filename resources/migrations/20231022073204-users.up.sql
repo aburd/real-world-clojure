67e9f1ad1084d6a7a26df7cb2c07fc537d2e707d
Aaron Burdick <aaron.burdick@protonmail.com>
2023-10-26 18:22:37 +0900

    fix: email column

--- resources/migrations/20231022073204-users.up.sql
+++ resources/migrations/20231022073204-users.up.sql
@@ -1,6 +1,6 @@
 create table if not exists users(
   id int generated always as identity primary key,
-  email text not null,
+  email text not null unique,
   password_hash text not null,
   token text,
   created_at timestamp default current_date not null,
@@ -10,7 +10,7 @@ create table if not exists users(
 create table if not exists profiles(
   id int generated always as identity primary key,
   user_id bigint not null,
-  username text not null,
+  username text not null unique,
   bio text,
   image text,
   foreign key(user_id)
